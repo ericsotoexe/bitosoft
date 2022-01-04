c11dbdbac50e4ad58d5a038ef4ebf680057c6148
Eric Soto <ericsoto.exe@gmail.com>
2022-01-04 16:06:29 -0400

    Web 0.1.83v

--- main.js
+++ main.js
@@ -34,22 +34,22 @@ start();
 
 function createBreedList(breedList) {
   document.getElementById("breed").innerHTML = `
-    <select onchange="loadByBreed(this.value)">
+  <select onchange="loadByBreed(this.value)">
         <option>Choose a dog breed</option>
         ${Object.keys(breedList)
           .map(function (breed) {
             return `<option>${breed}</option>`;
           })
           .join("")}
-    </select>
-    `;
+      </select>
+  `;
 }
 
 async function loadByBreed(breed) {
   if (breed != "Choose a dog breed") {
     const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
     const data = await response.json();
-    console.log(data);
+    createSlideshow(data.message);
   }
 }
 
